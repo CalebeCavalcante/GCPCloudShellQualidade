1b207635871ef6fb07b7fe47188afba2389467a3
Calebe Cavalcante <calebesantos.cavalcante@gmail.com>
2023-06-06 00:47:46 -0300

    Ajuste Import JSON Turbina

--- import_turbina_json.sh
+++ import_turbina_json.sh
@@ -1,5 +1,4 @@
-bq load --location=us-east1 --project_id=tim-sdbx-qualidade-2a75 \
- --noautodetect --source_format=NEWLINE_DELIMITED_JSON \ 
- dm_prod.car_turbina_filtro_generico \
+bq load --location=us-east1 \
+ --noautodetect --source_format=NEWLINE_DELIMITED_JSON tim-sdbx-qualidade-2a75:dm_prod.car_turbina_filtro_generico \
  gs://tim-sdbx-qualidade-2a75-temp-data/REPORT_DIARIO/tb_turbina_geral.json \
  ~/GCPCloudShellQualidade/schemas/car_turbina_filtro_generico.json
