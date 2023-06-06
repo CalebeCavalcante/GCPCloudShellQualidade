29888bc8f1d3097f6586733198e9c6caec9cd611
Calebe Cavalcante <calebesantos.cavalcante@gmail.com>
2023-06-06 01:40:16 -0300

    Ajustes Import Anatel

--- import_reclamacoes_anatel.sh
+++ import_reclamacoes_anatel.sh
@@ -1,5 +1,5 @@
 bq load --location=us-east1 --project_id=tim-sdbx-qualidade-2a75 \
- --noautodetect --allow_quoted_newlines --field_delimiter="|" \
- --skip_leading_rows=1 --source_format=CSV dm_prod.car_anatel_reclamacoes_pesquisa_avancada \
+ --noautodetect --allow_quoted_newlines --field_delimiter=";" \
+ --skip_leading_rows=1 --encoding="ISO-8859-1" --source_format=CSV dm_temp.car_anatel_reclamacoes_pesquisa_avancada \
  gs://tim-sdbx-qualidade-2a75-temp-data/REPORT_DIARIO/Relatorio_Solicitacoes_*.csv \
  ~/GCPCloudShellQualidade/schemas/car_reclamacoes_anatel.json
