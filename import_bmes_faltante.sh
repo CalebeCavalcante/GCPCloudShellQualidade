2838dcc39fea9a11a61fa8c27564d62d2d2edc3d
Calebe Cavalcante <calebesantos.cavalcante@gmail.com>
2023-06-12 22:00:27 -0300

    Ajuste Import Bmes

--- import_bmes_faltante.sh
+++ import_bmes_faltante.sh
@@ -1,5 +1,5 @@
 bq load --location=us-east1 --project_id=tim-sdbx-qualidade-2a75 \
- --encoding="ISO-8859-1" --noautodetect --allow_quoted_newlines --field_delimiter="|" \
+ --encoding="ISO-8859-1" --max_bad_records=100000 --noautodetect --allow_quoted_newlines=true --field_delimiter="|" \
  --skip_leading_rows=1 --replace=true --source_format=CSV dm_temp.car_bmes_faltante \
  gs://tim-sdbx-qualidade-2a75-temp-data/REPORT_DIARIO/BMES_AZURE_TO_GCP*.txt \
  ~/GCPCloudShellQualidade/schemas/car_bmes_faltante.json
