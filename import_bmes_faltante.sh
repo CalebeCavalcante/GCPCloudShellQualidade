7a26edf58785c358d95fbb46c2a1be2b322a523c
Calebe Cavalcante <calebesantos.cavalcante@gmail.com>
2023-06-12 21:31:26 -0300

    encoding Bmes

--- import_bmes_faltante.sh
+++ import_bmes_faltante.sh
@@ -1,6 +1,5 @@
-tim-sdbx-qualidade-2a75.dm_temp.car_bmes_faltante
 bq load --location=us-east1 --project_id=tim-sdbx-qualidade-2a75 \
- --noautodetect --allow_quoted_newlines --field_delimiter="|" \
+ --encoding="ISO-8859-1" --noautodetect --allow_quoted_newlines --field_delimiter="|" \
  --skip_leading_rows=1 --replace=true --source_format=CSV dm_temp.car_bmes_faltante \
  gs://tim-sdbx-qualidade-2a75-temp-data/REPORT_DIARIO/BMES_AZURE_TO_GCP*.txt \
  ~/GCPCloudShellQualidade/schemas/car_bmes_faltante.json
